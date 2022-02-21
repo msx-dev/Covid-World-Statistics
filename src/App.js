96cebc3680cbb82444c263a444dbad43af9df609
MS <ms@MSs-MacBook-Air.local>
2022-02-21 21:10:26 +0100

    Cleaned the project and added a dropdown MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>Cleaned project</h1>
+        <FormControl>
+          <Select variant="outlined" value="abc">
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+            <MenuItem value="Daily">Daily</MenuItem>
+            <MenuItem value="Monthly">Monthly</MenuItem>
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
